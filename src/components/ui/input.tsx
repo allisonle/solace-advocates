0d28a5287e42636c87cad15d32534fc4cd0b9e60
Allison Le <hello@allisonle.com>
2024-11-03 00:45:34 +0900

    Add optional start and end elements to Input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,25 +1,39 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  startElem?: React.ReactNode;
+  endElem?: React.ReactNode;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, startElem, endElem, ...props }, ref) => {
     return (
-      <input
-        type={type}
+      <div
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          "flex items-center justify-between gap-2 h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm [&:has(:focus-visible)]:outline-none [&:has(:focus-visible)]:ring-2 [&:has(:focus-visible)]:ring-ring [&:has(:focus-visible)]:ring-offset-2 ",
+          className,
         )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
+      >
+        <div className="flex w-full items-center gap-2">
+          {startElem && startElem}
+          <input
+            type={type}
+            className={cn(
+              "w-full ring-offset-background outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+              className,
+            )}
+            ref={ref}
+            {...props}
+          />
+        </div>
+        {endElem && endElem}
+      </div>
+    );
+  },
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
