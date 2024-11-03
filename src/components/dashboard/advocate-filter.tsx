00bb5ec42268f9110e25c24c8dd8fca85b1e3dda
Allison Le <hello@allisonle.com>
2024-11-03 15:27:43 +0900

    Add button to clear search filter

--- src/components/dashboard/advocate-filter.tsx
+++ src/components/dashboard/advocate-filter.tsx
@@ -10,13 +10,24 @@ interface Props {
 
 const AdvocateFilter: FC<Props> = ({ searchFilter, setSearchFilter }) => {
   return (
-    <div className="w-1/3 flex flex-row gap-2 my-4 items-center">
+    <div className="w-1/3 flex flex-row gap-2 mb-4 px-4 items-center">
       <Input
+        className="bg-white border-border"
         placeholder="Search for an advocate..."
         type="text"
         value={searchFilter}
         onChange={e => setSearchFilter(e.target.value)}
         startElem={<Search size={18} />}
+        endElem={
+          searchFilter && (
+            <button
+              className="cursor-pointer"
+              onClick={() => setSearchFilter("")}
+            >
+              <X size={18} />
+            </button>
+          )
+        }
       />
     </div>
   );
